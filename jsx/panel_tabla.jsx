f5b0d9194ad06d76a0080c2729476f4f51be4d24
Eneas Gallego <eneasgallego@gmail.com>
2016-03-25 15:21:03 +0100

    Calculo alto de la tabla en el Panel

--- jsx/panel_tabla.jsx
+++ jsx/panel_tabla.jsx
@@ -1,6 +1,7 @@
 window.PanelTabla = React.createClass({
 	getInitialState: function() {
     	return {
+			alto_tabla: undefined
     	};
   	},
 	getDefaultProps: function() {
@@ -14,25 +15,46 @@ window.PanelTabla = React.createClass({
 			acciones: [],
 			claseFila: function(){},
 			parseData: function(){},
+			onResizeTabla: function(){},
 			onClickAcciones: function(){}
 		};
 	},
-	componentWillUpdate: function() {
+	componentDidMount: function() {
+	},
+	onResizeTabla: function (offset, tabla) {
+		var dom = ReactDOM.findDOMNode(this);
+		var alto_panel = dom.offsetHeight;
+		var dom_titulo = ReactDOM.findDOMNode(this.refs.titulo);
+		var alto_titulo = dom_titulo.offsetHeight;
+		var alto = alto_panel - alto_titulo;
+
+		this.setState({alto_tabla: alto_tabla});
+	},
+	onClickAcciones: function (tag, fila, tabla) {
+		this.props.onClickAcciones(tag, fila, tabla, this);
 	},
 	parseData: function (data, tabla) {
 		var ret = this.props.parseData(data, tabla, this);
 
 		return ret ? ret : data;
 	},
-	onClickAcciones: function (tag, fila, tabla) {
-		this.props.onClickAcciones(tag, fila, tabla, this);
-	},
 	refrescar: function () {
 		this.refs.tabla.refrescar();
+	},
+	renderStyleTabla: function () {
+
+		var ret = {};
+
+		if (this.state.alto_tabla) {
+			ret.height = this.state.alto_tabla + 'px';
+		}
+
+		return ret;
 	},
 	renderTabla: function () {
 		this.tabla = <Tabla
 			ref="tabla"
+			style={this.renderStyleTabla()}
 			id_campo={this.props.id_campo}
 			url={this.props.url}
 			parseData={this.parseData}
@@ -40,6 +62,7 @@ window.PanelTabla = React.createClass({
 			claseFila={this.props.claseFila}
 			cols={this.props.cols}
 			acciones={this.props.acciones}
+			onResize={this.onResizeTabla}
 			onClickAcciones={this.onClickAcciones}
 		/>
 
@@ -48,7 +71,7 @@ window.PanelTabla = React.createClass({
 	render: function() {
 		return (
 			<section id={this.props.id} className="panel">
-				<h2>{this.props.titulo}</h2>
+				<h2 ref="titulo">{this.props.titulo}</h2>
 				{this.renderTabla()}
 			</section>
 		);
