1c5a838d9380e3ffbc0401743c68803a619079fa
Eneas Gallego <eneasgallego@gmail.com>
2016-04-17 20:01:18 +0200

    resize fila

--- jsx/fila.jsx
+++ jsx/fila.jsx
@@ -8,7 +8,7 @@ class Fila extends React.Component {
 	constructor(props) {
 		super(props);
 
-		if (props.onResizeCelda) props.onResizeCelda = props.onResizeCelda.bind(this);
+		if (props.onResizeCelda) this.onResizeCelda = this.onResizeCelda.bind(this);
 
 		this.state = {
 			cols: parseCols(props.cols)
